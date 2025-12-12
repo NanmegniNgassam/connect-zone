867428298b28d1ea98300e39c111956ca2e7b203
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-12-12 23:06:13 +0100

    Test: running tests on the new application

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('connect-zone');
+  protected readonly title = signal<string>('connect-zone');
 }
